219a85e7f4b571143eca096336ea6a026433ea3c
Louis Ware <louisware@SoliriusAdmin197s-MacBook-Pro.local>
2023-10-19 17:39:25 +0100

    Added 'show' function to Workout controller to display Workout on a new page when clicked on

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,4 +2,10 @@ class WorkoutsController < ApplicationController
     def index
         @workouts = Workout.all
     end
+
+    def show
+        @workout = Workout.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
 end
