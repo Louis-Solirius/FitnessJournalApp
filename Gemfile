2996ea4e9b752928c45c9f1ebb40fd8281111512
Louis Ware <louisware@SoliriusAdmin197s-MacBook-Pro.local>
2023-10-20 15:59:13 +0100

    Revert "Added functionality to edit and delete workouts"
    This reverts commit 66ea238ba2d2cacec00ec04e21cae60603f2162a.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem "rails", "~> 7.1.1"
 gem "sprockets-rails"
 
 # Use postgresql as the database for Active Record
-gem "pg", "~> 1.4.5"
+gem "pg", "~> 1.1"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", ">= 5.0"
@@ -67,5 +67,3 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
-
-gem "devise", "~> 4.9"
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :installs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :workouts
+  get "/workouts/new", to: "workouts#new", as: :new_workout
+  get "/workouts/:id", to: "workouts#show", as: :workout
+  post "/workouts", to: "workouts#create", as: :workouts
+  
 
   # Defines the root path route ("/")
   root "workouts#index"
