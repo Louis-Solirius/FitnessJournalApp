1733fcdd276a166d510aaf599b72eaa2368f65bd
Louis Ware <louisware@Soliriun197sMBP.localdomain>
2023-10-25 10:23:23 +0100

    Added User association with workouts which enables each individual user to only view their own workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,7 +1,8 @@
 class WorkoutsController < ApplicationController
 
-    #before_action :authenticate_user!, except: [:index, :show]
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :set_workout, except: [:index, :new, :create]
+    before_action :correct_user, only: [:edit, :update, :destroy, :show]
 
     def index
         @workouts = Workout.all
@@ -11,14 +12,16 @@ class WorkoutsController < ApplicationController
     end
 
     def new
-        @workout = Workout.new
+        #@workout = Workout.new
+        @workout = current_user.workouts.build
     end
 
     def create
-        @workout = Workout.new(workout_params)
+        #@workout = Workout.new(workout_params)
+        @workout = current_user.workouts.build(workout_params)
+
         if @workout.save
             redirect_to(@workout, :notice => 'New workout created')
-            #redirect_to @workout
         else
             render :new, status: :unprocessable_entity 
         end
@@ -37,13 +40,18 @@ class WorkoutsController < ApplicationController
 
     def destroy
         @workout.destroy
-        redirect_to root_path
+        redirect_to(root_path, :notice => 'Workout deleted')
+    end
+
+    def correct_user
+        @workout = current_user.workouts.find_by(id: params[:id])
+        redirect_to workouts_path, notice: "Not Authorised to view or edit this workout" if @workout.nil?
     end
 
     private
 
     def workout_params
-        params.require(:workout).permit(:date, :notes, :title) 
+        params.require(:workout).permit(:date, :notes, :title, :user_id) 
     end
 
     def set_workout
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,7 @@
 class Workout < ApplicationRecord
+
+    belongs_to :user
+
     validates :date, presence: true
     validates :notes, presence: true
     validates :title, presence: true
